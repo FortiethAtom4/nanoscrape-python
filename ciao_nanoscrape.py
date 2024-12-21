215247b2c1983e89d07eba6ea4c545682857ae5b
Zirconium <txbbrand@gmail.com>
2024-12-21 04:53:52 -0500

    fixed requirements.txt, polished ciao-nanoscrape (still needs some error handling)

--- ciao_nanoscrape.py
+++ ciao_nanoscrape.py
@@ -14,59 +14,54 @@ from objs.scrape_config import ScrapeConfig
 # Scraper module intended to scrape raws of NekoMeru from Ciao.
 # Example link: https://ciao.shogakukan.co.jp/comics/title/00511/episode/27493
 
-#  ~ this is gonna be a lot uglier than the kowloon scraper module ~
-
-# No matter what, I'm going to have to simulate flipping pages to access all of the pages in each chapter. 
 # canvas_selector = ".c-viewer__comic"
 # navigation_selector = ".c-viewer__pager-next"
 # page_container = ".c-viewer__pages"
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("~~~~~~~~~~NANOSCRAPE~~~~~~~~~~")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("nanoscrape-python version 12/2024.\n")
 
 scrape_config = ScrapeConfig("ciao-nanoscrape","c-viewer__comic")
 scrape_config.from_args() # get command line arguments
 
 options = uc.ChromeOptions()
 options.add_argument("--disable-web-security")
-# options.set_preference("security.mixed_content.block_display_content",True)
-# options.set_preference("browser.ssl_override_behavior",2)
-
-    # browser.ssl_override_behavior = 2
-
-    # browser.xul.error_pages.expert_bad_cert = false
-
-    # security.ssl.errorReporting.enabled = false
-
-# options.add_argument("--headless")
+options.add_argument("--headless")
 # add options here, preferably from scrape_config
 driver = uc.Chrome(options=options)
 images: list = []
+
+print(f"Opening {scrape_config.url}...")
 driver.get(scrape_config.url)
 
 # wait until page content is actually loaded
-pages_present = expected_conditions.element_to_be_clickable((By.CLASS_NAME,"c-viewer__pager-next"))
+pages_present = expected_conditions.element_to_be_clickable((By.CLASS_NAME,"c-viewer__pager-next")) # TODO catch wait errors for bad links
 timeout = 10 # seconds to wait until timeout
 WebDriverWait(driver, timeout).until(pages_present)
 time.sleep(5) # had to add 5 seconds after pages present to catch everything
 
+print("-> Images detected.")
 #count number of pages
 all_pages = driver.find_elements(By.CLASS_NAME,scrape_config.image_selector)
-print(len(all_pages))
 
 # to store images
+print("Beginning scrape...")
 scrape_config.make_directory()
 try:
-# get images
-    for i in range(0, len(all_pages)):
+# get images, loop runs until elem without canvas is reached
+    for i, page in enumerate(all_pages):
         all_pages = driver.find_elements(By.CLASS_NAME,scrape_config.image_selector) # needs to be updated each time
         canvas_element = ""
 
         # end of chapter has pages without canvas, use those to break loop
         try:
-            canvas_element = all_pages[i].find_element(By.TAG_NAME,"canvas")
+            canvas_element = page.find_element(By.TAG_NAME,"canvas")
         except NoSuchElementException:
-                print("End of chapter found, breaking loop")
+                print("\nEnd of chapter found, breaking loop")
                 break
         canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png',quality=1).substring(21);",canvas_element)
-        print(f"Found image {i + 1}...")
+        print(f"Got {i + 1} image{"" if i == 0 else "s"}...",end="\r")
         canvas_png = base64.b64decode(canvas_base64)
         images.append(canvas_png)
 
@@ -74,14 +69,17 @@ try:
         if i % 2 == 0:
             next_page = driver.find_element(By.CLASS_NAME,"c-viewer__pager-next")
             next_page.click()
+            time.sleep(0.1)
     
 except Exception as e:
     print(f"\nAn error was caught during scraping:\n{e}\nAborting")
 finally:
     driver.quit()
-    for id, img in enumerate(images):
-        print(f"Saving {id + 1}.png...")
-        with open(f"{scrape_config.directory}/{id + 1}.png","wb") as f:
-            f.write(img)
+    if len(images) > 0:
+        for id, img in enumerate(images):
+            print(f"Saving {id + 1}.png...")
+            with open(f"{scrape_config.directory}/{id + 1}.png","wb") as f:
+                f.write(img)
+        print(f"\nImages saved to local directory '{scrape_config.directory}/'.")
 
-    print("--Script complete--")
+    print("\n--Scrape complete--\n")
