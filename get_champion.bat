42ab9c98022c0b6b460e82e923e6c1565e3ba216
Thomson Brand <txbbrand@gmail.com>
2025-03-26 02:55:00 -0400

    Delete get_champion.bat

--- get_champion.bat
+++
@@ -1,4 +0,0 @@
-python champion_nanoscrape.py https://youngchampion.jp/episodes/cd6fbbd15fdef/ -d tests/sick_planet/22-1/raws
-python champion_nanoscrape.py https://youngchampion.jp/episodes/918efedfe057c/ -d tests/sick_planet/22-2/raws
-python champion_nanoscrape.py https://youngchampion.jp/episodes/65283c7b45163/ -d tests/sick_planet/final-1/raws
-python champion_nanoscrape.py https://youngchampion.jp/episodes/8d47618ec731e/ -d tests/sick_planet/final-2/raws
